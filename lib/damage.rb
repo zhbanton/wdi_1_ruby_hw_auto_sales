9c545aca2654ed7dd95e3abfd489268f8354996e
zhbanton <zhbanton@gmail.com>
2014-06-12 18:38:46 -0400

    Finished used car class and test methods

--- lib/damage.rb
+++ lib/damage.rb
@@ -2,6 +2,9 @@
 # has a cost (how much it would cost to repair)
 
 class Damage
+
+  attr_reader :cost
+
   def initialize (description, cost)
     @description = description
     @cost = cost
--- lib/used_car.rb
+++ lib/used_car.rb
@@ -4,18 +4,25 @@
 # has a value (same calculation as a new car, modified by mileage depreciation and the cost of all current damages)
 # all used cars have a constant mileage depreciation of 1% per 10,000 miles (0.0001% per mile)
 
-require_relative "car"
+require_relative 'car.rb'
+require_relative 'damage.rb'
 
 MILEAGE_DEPRECIATION_PERCENTAGE = 1
 MILEAGE_DEPRECIATION_MILES = 10000
 MILEAGE_DEPRECIATION = (MILEAGE_DEPRECIATION_PERCENTAGE.to_f / MILEAGE_DEPRECIATION_MILES)
 
 class UsedCar < Car
-  #def initialize (make, model, year, msrp, markup: 0)
+
   def initialize (make, model, year, msrp, mileage, damages, markup: 0)
-    super (make, model, year, msrp, markup)
+    super(make, model, year, msrp, markup: markup)
     @mileage = mileage
     @damages = damages
+  end
+
+  def value
+    damage_cost = 0
+    @damages.each { |damage| damage_cost += damage.cost }
+    super - (MILEAGE_DEPRECIATION * @mileage) - damage_cost
   end
 end
 
