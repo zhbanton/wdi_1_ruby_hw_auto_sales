1ad316d510b87d668d55978e25f4cc9e62bb2955
zhbanton <zhbanton@gmail.com>
2014-06-12 17:19:26 -0400

    Implement constructor in car class, test file

---
+++ bin/car_test.rb
@@ -0,0 +1,5 @@
+require_relative "../lib/car.rb"
+
+my_car = Car.new("Toyota", "Highlander", 2006, 35000, markup: 100)
+
+puts my_car.inspect
---
+++ lib/car.rb
@@ -0,0 +1,23 @@
+# has a make, model, and year of manufacture
+# has an MSRP (original retail price)
+# has a value (MSRP modified by depreciation, taking the year of manufacture into account)
+# all cars have a constant depreciation of 5% per year
+# has a manager markup
+# has a price (value modified by the manager markup)
+require 'date'
+
+class Car
+
+DEPRECIATION = 0.05
+
+
+  def initialize (make, model, year, msrp, markup: 0)
+    @make = make
+    @model = model
+    @year = year
+    @msrp = msrp
+    @markup = markup
+  end
+
+end
+
